95b04855139d4c1fcb4d6ce6d4ec45e8ae18d894
Chappie <ugurtosun50@gmail.com>
2020-06-07 01:02:37 +0300

    Project setting up.

---
+++ src/layout/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Content = () => (
+    <div className="content">
+
+    </div>
+);
+
+export default Content;
---
+++ src/layout/SinglePage/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+    <div className="homepage">
+
+    </div>
+);
+
+export default Home;
