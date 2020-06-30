8e9871925aeae153713349a9dd5bd49c225a3ac9
Chappie <ugurtosun50@gmail.com>
2020-06-30 23:15:50 +0300

    Pnotify added. User adding to local storage with uuid.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter} from "react-router-dom";
 
-import './assets/sass/app.scss';
-import './assets/base/bootstrap.min.css';
+import {useLocalStorage} from "./hooks/useLocalStorage";
 
 import Routes from "./Routes";
 
+import PNotify from 'pnotify/dist/es/PNotify';
+
+import 'pnotify/dist/PNotifyBrightTheme.css';
+import './assets/sass/app.scss';
+import './assets/base/bootstrap.min.css';
+
+import { v4 as uuid } from 'uuid';
+
+PNotify.defaults.delay = 2000;
+PNotify.defaults.stack = {
+    dir1: 'left',
+    dir2: 'up',
+    firstpos1: 25,
+    firstpos2: 25,
+    push: 'bottom',
+    context: document.body
+};
+
+
 const App = () => {
+
+    const [storageUser, setStorageUser] = useLocalStorage('user');
+
+    if (!storageUser) {
+        setStorageUser(uuid());
+    }
+
     return (
         <div className="App">
             <BrowserRouter>
