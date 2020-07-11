ca3df2d5002a6d3d47e59088141e22f8ea09e431
Chappie <ugurtosun50@gmail.com>
2020-07-11 16:53:48 +0300

    Render problem half  solved.

--- src/pages/WatchTogether.js
+++ src/pages/WatchTogether.js
@@ -1,41 +1,47 @@
 import * as React from 'react';
-import {useState, useEffect} from "react";
+import {useState, useLayoutEffect} from "react";
 
 import {useDispatch, useSelector} from "react-redux";
 import {getRoom} from "../redux/actions/room/roomAction";
-
-import {useLocalStorage} from "../hooks/useLocalStorage";
 
 import {CreateOrJoinRoom} from "../component/Room/CreateOrJoinRoom";
 import {Room} from "../component/Room/Room";
 
 export const WatchTogether = () => {
 
-    const [storageRoom, setStorageRoom] = useLocalStorage('room');
-    const [storageUser, setStorageUser] = useLocalStorage('user');
-
     const room = useSelector(state => state.room);
-
-    const [roomActive, setRoomActive] = useState(room.status);
-
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (room.status) {
-            setRoomActive(true);
-        } else if (storageRoom) {
+    const [active, setActive] = useState(false);
+
+    useLayoutEffect(() => {
+        if (localStorage.getItem('room') !== null) {
+            // Room already created but not get and now getting room
             dispatch(getRoom({
-                room_id: storageRoom,
-                user_id: storageUser,
+                room_id: JSON.parse(localStorage.getItem('room')),
+                user_id: JSON.parse(localStorage.getItem('user')),
             }));
-            setRoomActive(true);
         }
-    }, [storageRoom, room.status]);
+    }, []);
+
+    useLayoutEffect(() => {
+        if (room.data.room_id) {
+            // If room exists
+            setActive(true)
+        }
+    }, [room.data.room_id]);
+
+    useLayoutEffect(() => {
+        if (localStorage.getItem('room') !== null && room.status) {
+            // Room created and already have status and room storage
+            setActive(true);
+        }
+    }, [room.status]);
 
     return (
         <div className="watch-together">
             {
-                roomActive ? <Room/> : <CreateOrJoinRoom/>
+                active ? <Room/> : <CreateOrJoinRoom/>
             }
         </div>
     );
