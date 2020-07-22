07633177467139dd03ee1f6f9a556c19bacd2c95
Chappie <ugurtosun50@gmail.com>
2020-07-22 14:29:56 +0300

    New clean  algorithm added.

--- src/pages/WatchTogether.js
+++ src/pages/WatchTogether.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useState, useLayoutEffect} from "react";
+import {useState, useEffect} from "react";
 
 import {useDispatch, useSelector} from "react-redux";
 import {getRoom} from "../redux/actions/room/roomAction";
@@ -14,9 +14,19 @@ export const WatchTogether = () => {
 
     const [active, setActive] = useState(false);
 
-    useLayoutEffect(() => {
+    useEffect(() => {
+        if (room.status) {
+            setActive(true);
+        } else if (!room.data.room_id && localStorage.getItem('room') === null) {
+            setActive(false);
+        } else {
+            setActive(false);
+        }
+    }, [room.status]);
+
+    useEffect(() => {
         if (localStorage.getItem('room') !== null) {
-            // Room already created but not get and now getting room
+            // getting room
             dispatch(getRoom({
                 room_id: JSON.parse(localStorage.getItem('room')),
                 user_id: JSON.parse(localStorage.getItem('user')),
@@ -24,24 +34,10 @@ export const WatchTogether = () => {
         }
     }, []);
 
-    useLayoutEffect(() => {
-        if (room.data.room_id) {
-            // If room exists
-            setActive(true)
-        }
-    }, [room.data.room_id]);
-
-    useLayoutEffect(() => {
-        if (localStorage.getItem('room') !== null && room.status) {
-            // Room created and already have status and room storage
-            setActive(true);
-        }
-    }, [room.status]);
-
     return (
         <div className="watch-together">
             {
-                active ? <Room/> : <CreateOrJoinRoom/>
+                active && room.data.room_id ? <Room/> : <CreateOrJoinRoom/>
             }
         </div>
     );
