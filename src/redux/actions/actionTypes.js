7599b8cadc375f26f03d620f3b773e480eaafad8
Chappie <ugurtosun50@gmail.com>
2020-07-22 14:30:21 +0300

    New actions types added.

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -3,7 +3,11 @@ export const SIDEBAR_TOGGLE = 'SIDEBAR_TOGGLE';
 
 /* ROOM TYPES */
 export const ROOM_CREATE = 'ROOM_CREATE';
+export const ROOM_JOIN = 'ROOM_JOIN';
 export const ROOM_GET = 'ROOM_GET';
+export const ROOM_SET = 'ROOM_SET';
+export const ROOM_DELETE_OR_EXIT = 'ROOM_DELETE_OR_EXIT';
 
 /* VIDEO TYPES */
 export const VIDEO_ON_PLAYING = 'VIDEO_ON_PLAYING';
+export const VIDEO_ON_SYNC = 'VIDEO_ON_SYNC';
