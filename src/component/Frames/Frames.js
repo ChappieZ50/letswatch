99ab6779794c18088db83e9c05433587bf3ed62e
Chappie <ugurtosun50@gmail.com>
2020-07-22 14:25:41 +0300

    New extension imported

--- src/component/Frames/Frames.js
+++ src/component/Frames/Frames.js
@@ -3,9 +3,11 @@ import * as React from 'react';
 import {YoutubeExtension} from "./Extensions/YoutubeExtension";
 import {TwitchExtension} from "./Extensions/TwitchExtension";
 import {useState} from "react";
+import {CopyRoomExtension} from "./Extensions/CopyRoomExtension";
+import {SyncExtension} from "./Extensions/SyncExtension";
+import {CloseRoomExtension} from "./Extensions/CloseRoomExtension";
 
 export const Frames = () => {
-
 
     const [frame, setFrame] = useState({
         type: '',
@@ -21,6 +23,9 @@ export const Frames = () => {
             <div className="open-frame-items">
                 <YoutubeExtension onClick={handleFrame}/>
                 <TwitchExtension onClick={handleFrame}/>
+                <CopyRoomExtension/>
+                <SyncExtension/>
+                <CloseRoomExtension/>
             </div>
         </div>
     );
