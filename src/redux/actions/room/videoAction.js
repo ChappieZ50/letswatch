6aacb5b7b07570f1e5327ac08374a78c9dce7423
Chappie <ugurtosun50@gmail.com>
2020-07-22 14:30:39 +0300

    Player sync added

--- src/redux/actions/room/videoAction.js
+++ src/redux/actions/room/videoAction.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import PNotify from 'pnotify/dist/es/PNotify';
 
 import {API} from "../../../config";
-import {VIDEO_ON_PLAYING} from "../actionTypes";
+import {VIDEO_ON_PLAYING, VIDEO_ON_SYNC} from "../actionTypes";
 
 export const onPlaying = state => () => {
     axios.post(API + '/video-actions/on-playing', state).catch(() => {
@@ -17,6 +17,15 @@ export const setPlaying = ({playing, seek, user_id}) => dispatch => {
             playing,
             seek,
             user_id
+        }
+    });
+};
+
+export const syncPlayer = ({sync}) => dispatch => {
+    return dispatch({
+        type: VIDEO_ON_SYNC,
+        payload: {
+            sync
         }
     });
 };
