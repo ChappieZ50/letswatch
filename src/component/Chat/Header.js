4634d8d8926a389179e20393f7a8501aec3efbee
Chappie <ugurtosun50@gmail.com>
2020-07-31 00:38:24 +0300

    User getting to chat header

--- src/component/Chat/Header.js
+++ src/component/Chat/Header.js
@@ -1,18 +1,20 @@
 import * as React from 'react';
 
-import {CloseChat} from "./Partials/CloseChat";
+import {useSelector} from "react-redux";
 
-import avatarFemale from "../../assets/images/avatars/female.svg";
+import {CloseChat} from "./Partials/CloseChat";
 import {Owner} from "../Owner";
 
+import avatarFemale from "../../assets/images/avatars/female.svg";
+import avatarMale from "../../assets/images/avatars/male.svg";
+
 export const Header = () => {
-
-
+    const user = useSelector(state => state.room.user);
     return (
         <div className="chat-header">
             <div className="chat-header-info">
-                <img src={avatarFemale} className="chat-avatar rounded-circle rounded" alt="Chappie"/>
-                <span className="chat-username">Chappie <Owner/></span>
+                <img src={user.gender === 'male' ? avatarMale : avatarFemale} className="chat-avatar rounded-circle rounded" alt="Chappie"/>
+                <span className="chat-username">{user.username} {user.owner ? <Owner/> : ''}</span>
             </div>
             <CloseChat/>
         </div>
