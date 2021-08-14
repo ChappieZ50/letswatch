0ad21ae8e69eae5832203a93d3489fc24ae11265
Uğur <ugurtosun50@gmail.com>
2021-08-14 19:06:51 +0300

    Update

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,12 @@ import {Route, Switch} from 'react-router-dom';
 import Landing from "./pages/Landing";
 import {WatchTogether} from "./pages/WatchTogether";
 import Layout from "./layout/Layout";
-import {Discussions} from "./pages/Discussions";
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Landing}/>
         <Layout>
             <Route path="/watch-together" component={WatchTogether}/>
-            <Route path="/discussions" component={Discussions}/>
         </Layout>
     </Switch>
 );
