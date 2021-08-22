c79e1d690fd75254cbea4c5f45300ca71fec5592
Uğur <ugurtosun50@gmail.com>
2021-08-22 17:04:12 +0300

    Redux and devtools disabled

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,6 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/index';
 
-const allEnhancers = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const allEnhancers = compose(applyMiddleware(thunk)/*, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/);
 
 export default createStore(rootReducer,allEnhancers);
